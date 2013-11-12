902df644222bf6d175ab926b1c11bd39f9a9470b
Aria Bennett <twoplustwo@gmail.com>
2013-11-11 17:32:21 -0800

    Initial fully working version.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ meteor-directory-builder
 ========================
 
 A bash script that builds a directory structure suited for meteor apps.
+
+Usage:
+
+./mdb.sh (destination) // leave destination blank to build the directories
+                          in the same directory as mdb.sh
---
+++ mdb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash          
+path=$1
+mkdir -p ./$path/client/css
+mkdir -p ./$path/client/js
+mkdir -p ./$path/client/lib/helpers
+mkdir -p ./$path/client/lib/meteor
+mkdir -p ./$path/client/views
+mkdir -p ./$path/server/api
+mkdir -p ./$path/server/config
+mkdir -p ./$path/server/helpers
+mkdir -p ./$path/server/lib
+mkdir -p ./$path/server/models
+mkdir -p ./$path/server/publish
+mkdir -p ./$path/lib/helpers
+mkdir -p ./$path/lib/models
+mkdir -p ./$path/packages
+mkdir -p ./$path/private
+mkdir -p ./$path/public/fonts
+mkdir -p ./$path/public/img
+mkdir -p ./$path/public/js
+mkdir -p ./$path/tests
